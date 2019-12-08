6296069353372ef241a8dfb69c8faca87a0c25c2
earthctzn <earthctzn1@gmail.com>
2019-12-07 22:20:15 -0500

    Adding index, createand update methods to games controller.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,3 +1,26 @@
 class GamesController < ApplicationController
-  
+  def index
+    @games = Game.all
+
+    render json: @games, status: 200
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      render json: @game. status: 200
+    end
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    if @game.update
+      render json: @game, status: 200
+    end
+  end
+
+  private
+    def game_params
+      params.require(:game).permit(:score)
+    end
 end
