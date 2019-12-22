0aa52af96067843448c19f546dd1f91d390117a3
earthctzn <earthctzn1@gmail.com>
2019-12-22 00:47:50 -0500

    Including player in create method of GamesController.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,15 +1,14 @@
 class Api::V1::GamesController < ApplicationController
   def index
-    @games = Game.all
-
-    render json: @games, status: 200
+    games = Game.all
+    render json: games, status: 200
   end
 
   def create
     player = Player.find_or_create_by(name: params[:player_name])
     game = Game.new(player: player, score: params[:score])
     if game.save
-      render json: game, status: 200
+      render json: game, include: [:player], status: 200
     else
       render json: {message: "error on create"}
     end
