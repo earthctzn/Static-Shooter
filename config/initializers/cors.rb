6bb4bb218488780224c2f66bb4fe95ecbdc0148f
earthctzn <earthctzn1@gmail.com>
2020-04-22 19:17:36 -0400

    updates cors.rb file origins with heroku url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://staticshooter.herokuapp.com/'
+    origins 'https://staticshooter.herokuapp.com'
 
     resource '*',
       headers: :any,
